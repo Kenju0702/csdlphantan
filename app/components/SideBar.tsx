21c352b4441f43e3d814916a21977d064e6ff457
QuangTien <nguyenquangtienvn2022@gmail.com>
2024-12-09 09:09:56 +0700

    oke

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, FileTextOutlined, GiftOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
@@ -11,23 +12,30 @@ type SidebarProps = {
 
 export const Sidebar: React.FC<SidebarProps> = ({ collapsed, onCollapse }) => {
   return (
-    <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} className="bg-gray-900">
+    <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} className="bg-gray-900" trigger={
+      <span
+      className="flex items-center justify-center cursor-pointer w-full text-2xl mt-2"
+    >
+      {collapsed ? '⬅️' : '➡️'}
+    </span>
+    
+    } >
       <div className="logo h-12 flex items-center justify-center text-white text-lg font-bold">
         Logo
       </div>
       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1">
-          <span>Option 1</span>
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Dashboard
         </Menu.Item>
-        <Menu.Item key="2">
-          <span>Option 2</span>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          Employee
         </Menu.Item>
-        <SubMenu key="sub1" title={<span><span>User</span></span>}>
+        <SubMenu key="sub1" title={<span><span>Product</span></span>} icon={<GiftOutlined />}>
           <Menu.Item key="3">Tom</Menu.Item>
           <Menu.Item key="4">Bill</Menu.Item>
           <Menu.Item key="5">Alex</Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" title={<span><span>Team</span></span>}>
+        <SubMenu key="sub2" title={<span><span>Order</span></span>} icon={<FileTextOutlined />}>
           <Menu.Item key="6">Team 1</Menu.Item>
           <Menu.Item key="8">Team 2</Menu.Item>
         </SubMenu>
